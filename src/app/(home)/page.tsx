e78779f4944074dc80e498ec1e319b8fe1d54ea4
rai <raic_dev@proton.me>
2025-07-21 18:02:44 +0900

    refactor: Update Home component to fetch products with recommendation counts
    - Refactored the Home component to retrieve products from the database, including their recommendation counts.
    - Replaced tRPC calls with direct database queries using drizzle-orm for improved performance and data handling.
    - Enhanced product fetching logic to order products by recommendation counts, ensuring a better user experience.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,39 @@
 import { Hero } from "@/components/hero";
 import { Products } from "@/components/products/products";
 import { Products as ProductsType } from "@/types/product";
-import { trpc } from "@/trpc/server";
+import { desc, eq, getTableColumns, sql } from "drizzle-orm";
+import { db } from "@/lib/db";
+import { recommendations as recommendationsTable } from "@/lib/db/schema";
+import { products as productsTable } from "@/lib/db/schema";
 
 export default async function Home() {
-  const products = await trpc.getProducts({}) as ProductsType;
+  const recommendationCounts = db
+    .select({
+      productId: recommendationsTable.productId,
+      count: sql<number>`count(${recommendationsTable.id})`
+        .mapWith(Number)
+        .as("count"),
+    })
+    .from(recommendationsTable)
+    .groupBy(recommendationsTable.productId)
+    .as("recommendationCounts");
+
+  const qb = db
+    .select({
+      ...getTableColumns(productsTable),
+      recommendationCount: recommendationCounts.count,
+    })
+    .from(productsTable)
+    .leftJoin(
+      recommendationCounts,
+      eq(productsTable.id, recommendationCounts.productId)
+    )
+    .$dynamic();
+
+  const products = (await qb.orderBy(desc(recommendationCounts.count))).map((p) => ({
+    ...p,
+    recommendationCount: p.recommendationCount || 0,
+  })) as ProductsType;
 
   return (
     <>
